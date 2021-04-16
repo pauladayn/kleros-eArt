9e9c9216ceb7cdccc03419100e0d9b8ccb59a583
Paula Dayup <paudayup@gmail.com>
2021-04-16 14:47:31 -0300

    vuelvo a agregar kleros

--- ethereum/centralized-court.js
+++
@@ -1,8 +0,0 @@
-import Web3 from 'web3';
-import CentralizedCourt from './CentralizedCourt.json'
-
-export const courtInstance = address => {
-new Web3.eth.Contract(CentralizedCourt.abi, address)
-}
-
-//Create a dispute
